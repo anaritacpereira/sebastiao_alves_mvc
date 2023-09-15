dad9f1564fe97a1d30128b4b4103174a4d057972
Ana Rita Pereira <127841235+anaritacpereira@users.noreply.github.com>
2023-09-15 12:58:47 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!--
+
+    Autor: Ana Rita Pereira
+    Data Inicio: 13/02/2023
+    Data Ultima Atualizacao: 05/03/2023
+
+-->
+
+<?php
+
+//Globais
+require_once "GLOBAL.php";
+require_once "helpers/helpers_list.php";
+$pagina_actual = "home";
+
+//Parte Superior do Site
+require_once "components/header.php";
+
+//Parte do Meio do Site
+require_once "pages/home.php";
+
+//Parte Inferior do Site
+require_once "components/footer.php";
+
+?>
